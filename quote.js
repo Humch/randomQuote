f86b8b153099f727da2542fabd73ede57fa89222
Humch <fabienshlegel@yahoo.fr>
2018-01-13 09:40:14 +0100

    first commit \o/

---
+++ quote.js
@@ -0,0 +1,17 @@
+function displayQuote (data){
+    $("#quote").empty();
+    $("#quote").append(data.quote);
+}
+
+function updateQuote () {
+    console.log("bip!");
+    $.ajax( {
+        url:'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1',
+        dataType: 'json',
+        type:'POST',
+        headers: {"X-Mashape-Key": 'iYsih6jNbDmshdB4W1xdm9j03SAXp1Ylyt6jsn0imvnyFRu2qu', Accept: 'application/json','Content-Type': 'application/x-www-form-urlencoded',},
+        success: function(data) {
+            displayQuote(data);
+        }
+    });
+}
